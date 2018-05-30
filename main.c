9768372fec6c00d5bd5fdd50e37f79c20a30e5c5
Alexandre Pinho <lxpnh98@gmail.com>
2018-05-30 02:21:41 +0100

    Fuck yes. Finalmente, quase 4 horas e meia a lidar com pipes que estava a dar tudo mal, mas o redirecionamento do output para os distribuidores está feito.

--- main.c
+++ main.c
@@ -5,13 +5,33 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 #include "common.h"
 #include "graph.h"
 #include "llist.h"
 #include "parser.h"
 
-void cria_processos(GRAPH g);
+void make_input_fifos(GRAPH g);
+void make_output_fifos(GRAPH g);
+void execute_graph(GRAPH g);
+
+void print_outputs(GRAPH g) {
+    int tam = graph_get_nnodes(g);
+    char path[10];
+    int fd;
+    int nbytes;
+    char buf[1024];
+    for(int i = 0; i < tam; i++) {
+        fprintf(stderr, "Output %d:\n", i);
+        sprintf(path, "o%d", i);
+        fd = open(path, O_RDONLY);
+        while ((nbytes = read(fd, buf, 1024)) > 0) {
+            write(2, buf, nbytes);
+        }
+        close(fd);
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -27,8 +47,10 @@ int main(int argc, char *argv[]) {
     int fd = open(argv[1], O_RDONLY);
 
     parse_file(fd, &g, &l);
-
-    cria_processos(g);
+    make_input_fifos(g);
+    make_output_fifos(g);
+    execute_graph(g);
+    print_outputs(g);
 
     struct block *b;
     LLIST x = llist_clone(l);
@@ -36,32 +58,99 @@ int main(int argc, char *argv[]) {
         write(1, b->buf, b->size);
         llist_next(x);
     }
-
     exit(0);
 }
 
-void cria_processos(GRAPH g) {
+void make_input_fifos(GRAPH g) {
+    int tam = graph_get_nnodes(g);
+    char path[10];
+    for(int i = 0; i < tam; i++) {
+        if (((struct cmd *)graph_get_data(g, i))->input_from > 0) {
+            sprintf(path, "i%d", i);
+            fprintf(stderr, "Criou fifo i%d\n", i);
+            mkfifo(path, 0400);
+        }
+    }
+}
+
+void make_output_fifos(GRAPH g) {
+    int tam = graph_get_nnodes(g);
+    char path[10];
+    for(int i = 0; i < tam; i++) {
+        sprintf(path, "o%d", i);
+        fprintf(stderr, "Criou fifo o%d\n", i);
+        mkfifo(path, 0400);
+    }
+}
+
+void execute_distribuidor(GRAPH g, int i, int pipe[]);
+
+void execute_graph(GRAPH g) {
     int tam = graph_get_nnodes(g);
     pid_t pid;
+    char path[10];
+    int input_fd;
+    int output_pipe[2];
 
     for(int i = 0; i < tam; i++) {
-        /*
-        LLIST adj = graph_get_adj(g, i);
-        int *j;
-        while ( (j = (int *)(llist_get_data(adj))) ) {
-            llist_next(adj);
+
+        sprintf(path, "i%d", i);
+        // abrir e redirecionar fifo de input
+        if (((struct cmd *)graph_get_data(g, i))->input_from > 0) {
+            input_fd = open(path, O_RDONLY);
+            fprintf(stderr, "Abriu fifo '%s'.\n", path);
+            dup2(input_fd, 0);
         }
-        */
-        if((pid = fork()) < 0) {
-            exit(1);
-        }
-        if (pid == 0) {
+
+        // redirecionar output e distribuir
+        pipe(output_pipe);
+        execute_distribuidor(g, i, output_pipe);
+
+        // executar comando
+        if((pid = fork()) == 0) {
+            close(output_pipe[0]);
+            dup2(output_pipe[1], 1);
             LLIST arg_list = ((struct cmd *)graph_get_data(g, i))->args;
             char **argv = (char **)llist_to_array(arg_list);
+            fprintf(stderr, "A executar comando '%s'.\n", argv[0]);
             execvp(argv[0], argv);
             exit(1); // em caso de erro
         } else {
             wait(NULL);
+            close(output_pipe[1]);
         }
     }
 }
+
+void execute_distribuidor(GRAPH g, int i, int pipe[]) {
+    char path[10];
+    LLIST arg_list = llist_create();
+
+    // nome do programa
+    llist_add_tail(arg_list, "./distribuidor");
+
+    // ficheiro de output para escrever no ficheiro resultado
+    sprintf(path, "o%d", i);
+    llist_add_tail(arg_list, path);
+
+    // adicionar fifos em que escrever
+    LLIST adj = llist_clone(graph_get_adj(g, i));
+    int *j;
+    while ( (j = (int *)(llist_get_data(adj))) ) {
+        sprintf(path, "i%d", *j);
+        fprintf(stderr, "Argument: %s\n", path);
+        llist_add_tail(arg_list, mystrdup(path));
+        llist_next(adj);
+    }
+    llist_add_tail(arg_list, NULL);
+
+    // executar programa
+    char **argv = (char **)llist_to_array(arg_list);
+    if (fork() == 0) {
+        close(pipe[1]);
+        dup2(pipe[0], 0);
+        execvp(argv[0], argv);
+        exit(1); // em caso de erro
+    }
+}
+
